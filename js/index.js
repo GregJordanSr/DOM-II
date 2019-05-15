ffbcc9f607877aacfee4035ae529f778cc19c5b3
Greg Jordan <gregljordan@gmail.com>
2019-05-15 02:23:16 -0400

    Completed 8 spectacular event handlers

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@
 
 // 1. Window alert
 
-let page = window.addEventListener('load', e => {
-    alert(`Welcome To The Fun Bus, enjoy the ride!!!!!`)
-    page.preventDefault();
-});
+// let page = window.addEventListener('load', e => {
+//     alert(`WARNING!!! The colors on this page change rapidly!!!!`)
+    
+// });
 
 // 2. Change Background color
 
@@ -23,7 +23,59 @@ const bodyBGCB = document.querySelector('body').addEventListener('mouseleave', e
 let words = document.querySelector('body');
 
     words.addEventListener('mousemove', event => {
-        console.log("mousemove");
           event.target.style.color = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
-        })
+        });
     
+// 4.
+
+let button = document.querySelectorAll('.btn');
+
+button.forEach( item => { 
+  item.addEventListener('click', event => {
+  item.textContent = "We're the same";
+})});
+
+
+// 5. Nav
+
+let nav = document.querySelectorAll('a')
+
+nav.forEach( item => { 
+  item.addEventListener('dblclick', event => {
+  item.textContent = 'New label';
+})});
+
+// 6.
+let logo = document.querySelector('.logo-heading');
+document.addEventListener('scroll', event => {
+  logo.style.transition = 'all 4s ease'
+  logo.style.fontSize = '7rem';
+
+})
+
+// 7.
+
+let img = document.querySelectorAll('img');
+
+img.forEach(item => {
+  document.addEventListener('scroll', event => {
+    item.style.transition = 'all .3s ease'
+    item.style.opacity = 0;
+
+})});
+
+// 8.
+
+window.addEventListener('scroll', event => {
+  if (window.scrollY === 0) {
+    let img = document.querySelectorAll('img');
+    img.forEach(item => {
+        item.style.transition = 'all .3s ease'
+        item.style.opacity = 1;
+    })
+  }
+}) 
+    
+  
+  
+
